7fb73c6e758b1cd7de7624970aac412750b3a37e
habel it <habeltechs@gmail.com>
2023-04-27 15:56:16 +0200

    login hook with jwt

--- handlers/login_hook.js
+++ handlers/login_hook.js
@@ -1,11 +1,13 @@
 "use strict";
 const gql = require("graphql-tag");
 const apollo_client = require("../config/apollo");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res) => {
   const { privateKey } = JSON.parse(process.env.AUTH_SERVER_SECRET);
   console.log("private key", privateKey, req.body.input.objects);
   let { objects } = req.body.input;
+  console.log("haha",objects)
 
   //Login in to the authorizer
   let login_gql = gql`
@@ -41,6 +43,9 @@ module.exports = async (req, res) => {
       }
     }
   `;
+
+  console.log("login gql",login_gql)
+
   let { data } = await apollo_client.mutate({
     mutation: login_gql,
     variables: {
@@ -50,27 +55,37 @@ module.exports = async (req, res) => {
       },
     },
   });
+  console.log("data",data)
+  let at = data && data.login && data.login.access_token;
+  let ev = data && data.login && data.login.user && data.login.user.email_verified;
+  let uId = data && data.login && data.login.user && data.login.user.id;
 
-  console.log(data.login.user);
+  if (!at) throw new Error("Login failed");
 
-  if (!data.login.access_token) throw new Error("Login failed");
-
-  if (!data.login.user.email_verified)
+  if (!ev)
     throw new Error("Email is not Confirmed. Please Confirm your email");
 
   //TODO: Add to users in main schema
-  // payload = {
-  //   sub: data.intern[0].intern_id,
-  //   aud: "Street Art Platform",
-  //   iat: Math.floor(Date.now() / 1000),
-  //   "metadata": {
-  //     "roles": ["user"],
-  //   },
-  // };
+
+
+  let payload = {
+    sub: uId,
+    iat: Math.floor(Date.now() / 1000),
+    "metadata": {
+      "roles": ["user"],
+      "x-hasura-user-id":  uId,
+			"x-hasura-default-role": "user",
+    },
+  };
+
+  let token = jwt.sign(payload, privateKey, { algorithm: "RS256" })
 
   return res.send({
-    userId: "trial id",
-    authToken: "token",
-    accessToken: "access token",
+    userId: uId,
+    authToken: at,
+    accessToken: token
+    // userId: "data && data.login && data.login.user && data.login.user.id",
+    // authToken: "hello world",
+    // accessToken: "hello world"
   });
 };
