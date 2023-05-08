6af0e53755ed07e1d06858930d19770bee79e501
habel it <habeltechs@gmail.com>
2023-05-08 12:04:54 +0200

    sample: adding users to main schema table

--- handlers/login_hook.js
+++ handlers/login_hook.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
   const { privateKey } = JSON.parse(process.env.AUTH_SERVER_SECRET);
   console.log("private key", privateKey, req.body.input.objects);
   let { objects } = req.body.input;
-  console.log("haha",objects)
+  console.log("haha", objects);
 
   //Login in to the authorizer
   let login_gql = gql`
@@ -44,7 +44,7 @@ module.exports = async (req, res) => {
     }
   `;
 
-  console.log("login gql",login_gql)
+  console.log("login gql", login_gql);
 
   let { data } = await apollo_client.mutate({
     mutation: login_gql,
@@ -55,37 +55,57 @@ module.exports = async (req, res) => {
       },
     },
   });
-  console.log("data",data)
+  console.log("data", data);
+  let user = data && data.login && data.login.user && data.login.user;
   let at = data && data.login && data.login.access_token;
-  let ev = data && data.login && data.login.user && data.login.user.email_verified;
-  let uId = data && data.login && data.login.user && data.login.user.id;
+  let ev = user.email_verified;
+  let uId = user.id;
 
   if (!at) throw new Error("Login failed");
 
-  if (!ev)
-    throw new Error("Email is not Confirmed. Please Confirm your email");
+  if (!ev) throw new Error("Email is not Confirmed. Please Confirm your email");
 
   //TODO: Add to users in main schema
+  const cGql = gql`
+    mutation ($userData: users_insert_input!) {
+      copy: insert_users_one(
+        object: $userData
+        on_conflict: { constraint: users_email_key, update_columns: [] }
+      ) {
+        id
+      }
+    }
+  `;
 
+  let respObj = await apollo_client.mutate({
+    mutation: cGql,
+    variables: {
+      userData: {
+        email: user.email,
+        age: 18,
+        gender: user.gender,
+        name: `${user.family_name || ""} ${user.middle_name || ""} ${
+          user.given_name || ""
+        }`,
+      },
+    },
+  });
 
   let payload = {
     sub: uId,
     iat: Math.floor(Date.now() / 1000),
-    "metadata": {
-      "roles": ["user"],
-      "x-hasura-user-id":  uId,
-			"x-hasura-default-role": "user",
+    metadata: {
+      roles: ["user"],
+      "x-hasura-user-id": uId,
+      "x-hasura-default-role": "user",
     },
   };
 
-  let token = jwt.sign(payload, privateKey, { algorithm: "RS256" })
+  let token = jwt.sign(payload, privateKey, { algorithm: "RS256" });
 
   return res.send({
     userId: uId,
     authToken: at,
-    accessToken: token
-    // userId: "data && data.login && data.login.user && data.login.user.id",
-    // authToken: "hello world",
-    // accessToken: "hello world"
+    accessToken: token,
   });
 };
